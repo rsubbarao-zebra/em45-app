e448b033b9d6d3a7fd23ff0a3481c2aea8b0fb3d
rsubbarao-zebra <rsubbarao@zebra.com>
2026-06-04 16:43:23 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'em45-app-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+// Install Service Worker and cache assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
